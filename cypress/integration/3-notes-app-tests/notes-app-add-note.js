d66cbb76bacf3b9a173f40d60c51401084c8693c
GuiSanto <guiontheworks@gmail.com>
2022-03-24 21:32:55 +0000

    New tests added

--- cypress/integration/3-notes-app-tests/notes-app-add-note.js
+++ cypress/integration/3-notes-app-tests/notes-app-add-note.js
@@ -2,38 +2,44 @@
 describe('Tests to be done on "Notes App', () => {
     beforeEach(() => {
         cy.visit('http://localhost:8080/')
-    })
-   
-    it('Visits "Notes App" and check is title', () => {
+
         cy.get('h1').contains("Notes App")
     })
 
-
-    it('Visits "Notes App" and add two new note items to list of notes', () => {
+    it('Add 1 new note item to list of notes. The list of items must have 1 item', () => {
         cy.get('textarea')
             .type('Buy tomatos{enter}')
 
         cy.get('#add').click()
 
-
-        cy.get('textarea')
-            .type('Buy apples{enter}')
-
-         cy.get('#add').click()
+        cy.get('.notes').get('.box').should('have.length', 1)
     })
-    
-    it('Visits "Notes App" and add tow new note items to list of notes. The list of items must be two', () => {
-        cy.get('textarea')
-            .type('Buy tomatos{enter}')
+
+    it('Add empty item', () => {
 
         cy.get('#add').click()
 
+        cy.on('uncaught:exception', (err, runnable) => {
+            console.log("err :" + err)
+            console.log("runnable :" + runnable)
+            return false
+        })
 
-        cy.get('textarea')
-            .type('Buy bread{enter}')
+        cy.get('.notes').get('.box').should('have.length', 0)
+    })
 
-        cy.get('#add').click()
+    it('Add 1 new item with a changed color', () => {
 
-        cy.get('.notes').get('.box').should('have.length', 2)
+        
+    })
+
+    it('Add 1 new item and increase the size of the letter', () => {
+
+        
+    })
+
+    it('Add 1 new item and decrease the size of the letter', () => {
+
+        
     })
 })
