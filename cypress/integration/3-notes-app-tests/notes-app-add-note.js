6cde0135a7685a6d11b8f55a291e656a507a0cf6
GuiSanto <guiontheworks@gmail.com>
2022-03-25 11:18:49 +0000

    "Add" tests finished

--- cypress/integration/3-notes-app-tests/notes-app-add-note.js
+++ cypress/integration/3-notes-app-tests/notes-app-add-note.js
@@ -28,7 +28,7 @@ describe('Tests to be done on "Notes App', () => {
         cy.get('.box').contains("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
 
         /*
-            visualmente a textbox não está correta, era necessário usar Visual Testing
+            Visualmente a textbox não está correta, era necessário usar Visual Testing
         */
     })
 
@@ -46,8 +46,22 @@ describe('Tests to be done on "Notes App', () => {
     })
 
     it('Add 1 new item with a changed color to green', () => {
+        cy.get('textarea')
+            .type('New green item{enter}')
 
+        //change color to green
+        cy.get('input[type=color]')
+            .invoke('val', '#008000')
+            .trigger('change')
+
+        cy.get('#add').click()
         
+        //confirm item was added with the correct text
+        cy.get('.notes').get('.box').should('have.length', 1)
+        cy.get('.box').contains("New green item")
+
+        //confirm the color is the same the user choose
+        cy.get('p').should('have.css', 'color', 'rgb(0, 128, 0)')
     })
 
     it('Add 1 new item and increase the size of the letter to 30', () => {
@@ -60,9 +74,10 @@ describe('Tests to be done on "Notes App', () => {
 
         cy.get('.notes').get('.box').should('have.length', 1)
 
-        //confirm item was added with the correct text
+        //confirm item was added with the correct text and size
         cy.get('.notes').get('.box').should('have.length', 1)
         cy.get('.box').contains("Big text example")
+        cy.get('p').should('have.css', 'font-size', '30px')
     })
 
     it('Add 1 new item and decrease the size of the letter to 8', () => {
@@ -73,8 +88,9 @@ describe('Tests to be done on "Notes App', () => {
         cy.get('#size').type('8');
         cy.get('#add').click();
 
-        //confirm item was added with the correct text
+        //confirm item was added with the correct text and size
         cy.get('.notes').get('.box').should('have.length', 1)
         cy.get('.box').contains("Small text example")
+        cy.get('p').should('have.css', 'font-size', '8px')
     })
 })
