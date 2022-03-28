55d3f8f32cf4d7388eb47421eead5f1164edd195
GuiSanto <guiontheworks@gmail.com>
2022-03-28 16:30:13 +0100

    Added 2 new iterations to remove js

--- cypress/integration/3-notes-app-tests/notes-app-remove-note.js
+++ cypress/integration/3-notes-app-tests/notes-app-remove-note.js
@@ -18,7 +18,6 @@ describe('Tests to be done on "Notes App', () => {
         cy.get(':nth-child(1) > button').click()
 
         cy.get('.notes').get('.box').should('have.length', 0)
-
     })
 
     it('Remove an item from an empty list', () => {
@@ -26,6 +25,60 @@ describe('Tests to be done on "Notes App', () => {
 
         //since there are no items no remove, the button to remove should not exist
         cy.get(':nth-child(1) > button').should('not.exist')
+    })
+
+    it('Remove the last item from a list', () => {
+        
+        //add 3 items to a list
+        cy.get('textarea')
+            .type('Buy tomatos{enter}')
+
+        cy.get('#add').click()
+
+        cy.get('textarea')
+            .type('Buy oranges{enter}')
+
+        cy.get('#add').click()
+
+        cy.get('textarea')
+            .type('Buy pickles{enter}')
+
+        cy.get('#add').click()
+
+        //check it has 3 items
+        cy.get('.notes').get('.box').should('have.length', 3)
+
+        //remove the last item and check if it was removed
+        cy.get(':nth-child(3) > button').click()
+        cy.get('.notes').get('.box').should('have.length', 2)
+        cy.get('.box').not('.contains', "Buy pickles")
+    })
+
+    it('Remove the first item from a list', () => {
+        
+        //add 3 items to a list
+        cy.get('textarea')
+            .type('Buy tomatos{enter}')
+
+        cy.get('#add').click()
+
+        cy.get('textarea')
+            .type('Buy oranges{enter}')
+
+        cy.get('#add').click()
+
+        cy.get('textarea')
+            .type('Buy pickles{enter}')
+
+        cy.get('#add').click()
+
+        //check it has 3 items
+        cy.get('.notes').get('.box').should('have.length', 3)
+
+        //remove the first item and check if it was removed
+        cy.get(':nth-child(1) > button').click()
+        cy.get('.notes').get('.box').should('have.length', 2)
+        cy.get('.box').not('.contains', "Buy tomatos")
     })
 })
 
